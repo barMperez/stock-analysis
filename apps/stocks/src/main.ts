e60b0803fcbf07214c346707789f2233d56d9596
Bar Perez <barmikoperez@gmail.com>
2025-05-03 10:32:11 +0300

    Dockerized

--- apps/stocks/src/main.ts
+++ apps/stocks/src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(StocksModule);
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}));  
   app.useLogger(app.get(Logger));
   await app.listen(process.env.port ?? 3000);
 }
